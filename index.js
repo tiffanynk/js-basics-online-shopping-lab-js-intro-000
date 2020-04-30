ef0ede20850c5b73d8b0ad107832fc23d3fb8c4b
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-30 22:56:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function placeOrder(cardNumber) {
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
   else {
-    const cart = [];
+    cart = [];
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
 }
