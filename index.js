b75d70052fd2848be676980344b0ee524a77622b
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-30 21:44:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,12 +22,10 @@ function viewCart() {
     }
     else if (cart.length === 2) {
       return `${sent} ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
-    } else if (cart.length === 0) {
-      return "Your shopping cart is empty."
+    }
     }
   }
 
-}
 function total() {
   // write your code here
 }
