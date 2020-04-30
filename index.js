b1c5ff59c82186c098c6a99ce5fb634f2ed11bb2
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-30 22:38:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (i = 0; i < cart.length; i++) {
+  for (let i = 0; i < cart.length; i++) {
     if (item === cart[i].itemName) {
       cart.splice(i, 1);
       return cart;
