d73a0c6f233029c2f6a86f0b220e4fea1d056d2e
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-04-30 22:58:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   const cartTotal = total();
-  
+
     if (cardNumber === undefined) {
       return 'Sorry, we don\'t have a credit card on file for you.'
     }
